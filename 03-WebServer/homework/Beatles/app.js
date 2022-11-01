f6e20d2597fe73e740e7122341852281d4d62b86
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2022-11-01 15:21:32 -0300

    casi terminado

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -21,4 +21,77 @@ var beatles=[{
   birthdate: "07/08/1940",
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
-]
+];
+
+http.createServer(function(req, res){
+  if (req.url ==='/api' || req.url === '/api/'){
+    res.writeHead(200, { 'Content-Type':'application/json'});
+    res.end(JSON.stringify(beatles));
+  }; 
+
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type':'text/html'})
+    var html = fs.readFileSync(`${__dirname}/index.html`);
+    res.end(html);
+  };
+
+  if (req.url.substring(0,5) === '/api/' && req.url.length > 5) {
+    let findBeatle = req.url.split('/').pop();
+    let foundBeatle = beatles.find((beatle) => findBeatle === encodeURI(beatle.name));
+    if (foundBeatle){
+      // res.writeHead(200, {'Content-Type':'application/json'});
+      // res.end(JSON.stringify(foundBeatle));
+      res.writeHead(200, {'Content-Type':'application/json'})
+      res.end(JSON.stringify(foundBeatle));
+    } else {
+      res.writeHead(404, {'Content-Type':'text/plain'});
+      res.end('No existe ese Beatle');
+    }
+  };
+  
+
+
+  if (req.url.substring(0,5) === '/api/' && req.url.length > 5) {
+    let findBeatle = req.url.split('/').pop();
+    let foundBeatle = beatles.find((beatle) => findBeatle === encodeURI(beatle.name));
+    console.log(foundBeatle)
+    if (foundBeatle){
+      // res.writeHead(200, {'Content-Type':'application/json'});
+      // res.end(JSON.stringify(foundBeatle));
+      res.writeHead(200, {'Content-Type':'text/html'})
+      let html = fs.readFileSync(__dirname+ '/beatle.html', 'utf8');
+      html = html.replace(/{name}/g, foundBeatle.name);
+      html = html.replace('{birthdate}', foundBeatle.birthdate);
+      html = html.replace('{profilePic}', foundBeatle.profilePic);
+
+      //let finalHTML = replaceData(read, foundBeatle); --OTRA FORMA
+      res.end(html);
+    } else {
+      res.writeHead(404, {'Content-Type':'text/plain'});
+      res.end('No existe ese Beatle');
+    }
+  }
+}).listen(1337, '127.0.0.1');
+
+
+
+
+
+// http.createServer(function(req, res){
+//   if (req.url ==='/api' || req.url === '/api/'){
+//     res.writeHead(200, { 'Content-Type':'application/json'});
+//     res.end(JSON.stringify(beatles));
+//   } else if (req.url === `/api/${req.url}`){
+//     (err, data) => {
+//       if(err){
+//         res.writeHead(400, {'Content-Type':'text/plain'});
+//         res.end('Hubo un error')
+//       }
+//       else {
+//         res.writeHead(200, {'Content-Type':'application/json'});
+//         var obj = beatles['name'][req.url]
+//         res.end(JSON.stringify(obj))
+//       }
+//     }
+//   }
+// }).listen(1337, '127.0.0.1');
